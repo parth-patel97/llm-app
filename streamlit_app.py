cdda903ba6bd3dde379b8fc216650cf2581d9690
Parth Patel <parthpatel6037@gmail.com>
2023-08-27 16:39:39 +0530

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,26 +3,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Add a title and intro text
-st.title('Earthquake Data Explorer')
-st.text('This is a web app to allow exploration of Earthquake Data')
-
-# Create file uploader object
-upload_file = st.file_uploader('Upload a file containing earthquake data')
-
-# Check to see if a file has been uploaded
-if upload_file is not None:
-    # If it has then do the following:
-
-    # Read the file to a dataframe using pandas
-    df = pd.read_csv(upload_file)
-
-    # Create a section for the dataframe statistics
-    st.header('Statistics of Dataframe')
-    st.write(df.describe())
-
-    # Create a section for the dataframe header
-    st.header('Header of Dataframe')
-    st.write(df.head())
-
-    # Create a section for matplotlib figure
-    st.header('Plot of Data')
+st.title('Medical Symptom Checker')
+st.text('This is a web app to allow tell the medical condition and some remedies.')
