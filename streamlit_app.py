9e0491c430b318c3388ee6c7a53ac8b8bd817683
Parth Patel <parthpatel6037@gmail.com>
2023-08-30 09:43:54 +0530

    add the openai key

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # Add a title and intro text
 st.title('Medical Symptom Checker')
